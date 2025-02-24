3abead8f8e1e5a8fe9024521177a142c6336325a
Andressa <andressabtl@gmail.com>
2025-02-24 14:38:18 -0300

    Responsive version

--- src/layouts/Navigation.jsx
+++ src/layouts/Navigation.jsx
@@ -1,23 +1,29 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../assets/images/logo-text.png";
 import IconMagnifyingGlass from "../assets/icons/icon-magnifying-glass.svg";
+import IconBurgerMenu from "../assets/icons/icon-burger-menu.svg";
 
 const Navigation = () => {
+    const [navOpen, setNavOpen] = useState(false);
     return(
         <nav className="nav container">
-            <div className="nav-left">
-                <NavLink to="/" style={{height: "58px", padding: 0}}><img src={Logo} className="logo"/></NavLink>
-                <NavLink to="/properties?purpose=for-rent"><strong>Rent</strong></NavLink>
-                <NavLink to="/properties?purpose=for-sale"><strong>Buy</strong></NavLink>               
+            <NavLink to="/" style={{height: "58px", padding: 0}}><img src={Logo} className="logo"/></NavLink>
+            <div className={(navOpen == true ? "open":"") + " mobile-nav"}>
+                <div className="nav-left" onClick={() => setNavOpen(false)}>
+                    <NavLink to="/properties?purpose=for-rent"><strong>Rent</strong></NavLink>
+                    <NavLink to="/properties?purpose=for-sale"><strong>Buy</strong></NavLink> 
+                </div>              
+                <div className="nav-right" onClick={() => setNavOpen(false)}>
+                    <NavLink to="/agents">Agents</NavLink>
+                    <NavLink to="/calculator">Calculator</NavLink>
+                    <NavLink to="/properties?purpose=for-sale" style={{padding: 0}}>
+                        Search
+                        <img src={IconMagnifyingGlass} className="search-icon"/>
+                    </NavLink>
+                </div>
             </div>
-            <div className="nav-right">
-                <NavLink to="/agents">Agents</NavLink>
-                <NavLink to="/calculator">Calculator</NavLink>
-                <NavLink to="/properties?purpose=for-sale" style={{padding: 0}}>
-                    Search
-                    <img src={IconMagnifyingGlass} className="search-icon"/>
-                </NavLink>
-            </div>
+            <img src={IconBurgerMenu} className="burger-icon" onClick={() => setNavOpen(!navOpen)}/>
         </nav>
     )
 }
